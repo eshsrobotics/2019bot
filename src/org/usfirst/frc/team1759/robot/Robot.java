8ffaec173002d0afec844d3dc1a118c3e2263415
Spencer Moore <smoore_20@esusdstudents.org>
2018-01-13 01:03:00 +0000

    Added the CTRE_Phoenix library to the classpath. Does not work with SpeedControllerGroup. Requires more work.

--- src/org/usfirst/frc/team1759/robot/Robot.java
+++ src/org/usfirst/frc/team1759/robot/Robot.java
@@ -11,7 +11,10 @@ import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Talon;
+import com.ctre.phoenix.motorcontrol.can.*;
 
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
@@ -21,14 +24,28 @@ public class Robot extends IterativeRobot {
 	private DifferentialDrive m_myRobot;
 	private Joystick m_leftStick;
 	private Joystick m_rightStick;
-
+	private TalonSRX rightFront;
+	private TalonSRX rightBack;
+	private TalonSRX leftFront;
+	private TalonSRX leftBack;
+	private Spark s1;
+	private Spark s2;
+	private SpeedControllerGroup left;
+	private SpeedControllerGroup right;
+	private SpeedControllerGroup test;
 	@Override
 	public void robotInit() {
 		//Streams usb camera video feed straight to Dashboard.
 		CameraServer.getInstance().startAutomaticCapture();
 		
 		//Initialize drive.
-		m_myRobot = new DifferentialDrive(new Spark(0), new Spark(1));
+		rightFront = new TalonSRX(0);
+		rightBack = new TalonSRX(1);
+		leftFront = new TalonSRX(5);
+		leftBack = new TalonSRX(6);
+		//left = new SpeedControllerGroup(leftFront, leftBack);
+		//right = new SpeedControllerGroup(rightFront, rightBack);
+		m_myRobot = new DifferentialDrive(left, right);
 		m_leftStick = new Joystick(0);
 		m_rightStick = new Joystick(1);
 	}
