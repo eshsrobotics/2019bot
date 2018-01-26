f7b147319bac4e194ada9b338586a7bf7b8efa93
Aidan <asgalb15@gmail.com>
2018-01-25 17:06:53 -0800

    Edit launch code to be more general

--- src/main/java/org/usfirst/frc/team1759/robot/subsystems/Launcher.java
+++ src/main/java/org/usfirst/frc/team1759/robot/subsystems/Launcher.java
@@ -8,38 +8,30 @@ import edu.wpi.first.wpilibj.Solenoid;
 public class Launcher extends Subsystem {
 
 	Solenoid launcher;
-	
+
 	public Launcher() {
-		launcher = new Solenoid(0); 
+		launcher = new Solenoid(0);
 
 	}
+
 	@Override
 	public void setName(String subsystem, String name) {
 		// TODO Auto-generated method stub
-		
-		
-	} 
+
+	}
+
 	public void launch(OI oi) {
-		if (oi.joysticksAttached) {
-			if (oi.rightJoystick.getTrigger()) {
-				launcher.set(true);
-			} else {
-				launcher.set(false);
-			}
+		if (oi.highLaunch.get()) {
+			launcher.set(true);
 		} else {
-			if(oi.highLaunch.get()) {
-				launcher.set(true);
-			} else {
-				launcher.set(false); 
-			}
+			launcher.set(false);
 		}
 	}
 
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 }
