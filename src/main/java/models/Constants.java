9c4d837cd5a04ceca183d0e56d3524639b65ac75
Aidan <asgalb15@gmail.com>
2018-02-10 13:36:55 -0800

    uakotaobi committing on behalf of asgalb: Move the namespaces and extract a utility method.

--- src/main/java/models/Constants.java
+++ src/main/java/models/Constants.java
@@ -11,4 +11,6 @@ package models;
 public class Constants {
 	public static final double RADIANS_TO_DEGREES = 180 / Math.PI;
 	public static final double DEGREES_TO_RADIANS = Math.PI / 180;
+	
+	public static final Point ORIGIN = new Point(0, 0);
 }
--- src/main/java/org/usfirst/frc/team1759/robot/commands/Turn.java
+++ src/main/java/org/usfirst/frc/team1759/robot/commands/Turn.java
@@ -1,5 +1,7 @@
 package commands;
 
+import models.Constants;
+import models.Point;
 import models.Vector2;
 
 import org.usfirst.frc.team1759.robot.Sensors;
@@ -9,23 +11,50 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class Turn extends Command {
 
-	Vector2 myPosition;
 	TankDrive tank;
+	Vector2 myHeading;
 	
 	double myAngle;
 	double destAngle;
 	double degreesLeft;
 	
-	public void execute(TankDrive tank, Sensors sensors, Vector2 dest) {
-		
-		myPosition = new Vector2(0, 0);			//What do I use for the paramaters? Is this declared from a global myPosition Vector?
+	/**
+	 * This function is used to convert from the degrees that the gyro gives to a Vector2 heading.
+	 * @param gyroangle
+	 * @return
+	 */
+	private Vector2 gyroAngleToVector(double gyroangle) {
+		gyroangle = Sensors.gyro.getAngle();
+		myHeading = new Vector2(Math.cos(myAngleInRadians),		//X on a unit vector is cos(theta)
+					Math.sin(myAngleInRadians));	//Y on a unit vector is sin(theta)
+		return myHeading;
+	}
+	
+	/**
+	 * This function is used to turn the robot from its heading upon being called to the necessary heading to move to the next point.
+	 * 
+	 * @param tank
+	 * @param sensors
+	 * @param heading
+	 * 
+	 * Given a necessary heading, the robot will take input from the gyroscope to determine how far it needs to turn.
+	 * When the robot has a value, it will turn left if the difference in the angles is negative. 
+	 * 
+	 */
+	
+	public void start (TankDrive tank, Sensors sensors) {
 		tank = new TankDrive();
 		myAngle = Sensors.gyro.getAngle();
-		destAngle = myPosition.angle(dest);
+		Point startingPoint = Constants.ORIGIN;
+	}
+	
+	public void execute(Vector2 heading) {
+		
+		double myAngleInRadians = myAngle * Constants.DEGREES_TO_RADIANS;
+		destAngle = myHeading.angle(heading);	//Unit vectors should expect a magnitude of 1, with heading found as above.
 		
 		while (myAngle != destAngle) {
 			myAngle = Sensors.gyro.getAngle();
-			
 			degreesLeft = destAngle - myAngle;	//If degreesLeft is negative, we will want to go left. If degreesLeft is positive, we will want to go left.
 			
 			if(degreesLeft > 45) {				//If we are more than 45 degrees off, go quickly.
