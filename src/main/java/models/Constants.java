1569cae90bb3b2dbfd9526f2cd72baec7c01a2ef
Aidan <asgalb15@gmail.com>
2018-02-10 18:01:47 -0800

    Add Go function for movement to points directly ahead or behind
    The go function utilizes the accelerometer to get data about our position, which is calculated with a modified form of the kinematics. This iteratively uses these equations to change our current position and compare with our wanted position. With this data, it moves closer to the point until it is within an acceptable range. Comments are still needed.

--- src/main/java/models/Constants.java
+++ src/main/java/models/Constants.java
@@ -12,5 +12,7 @@ public class Constants {
 	public static final double RADIANS_TO_DEGREES = 180 / Math.PI;
 	public static final double DEGREES_TO_RADIANS = Math.PI / 180;
 	
+	public static final int MILLI_TO_SEC = 1 / 1000;
+	
 	public static final Point ORIGIN = new Point(0, 0);
 }
--- src/main/java/org/usfirst/frc/team1759/robot/commands/Go.java
+++ src/main/java/org/usfirst/frc/team1759/robot/commands/Go.java
@@ -1,8 +1,141 @@
 package org.usfirst.frc.team1759.robot.commands;
 
+import models.Constants;
+import models.Point;
+import models.Vector2;
+
+import org.usfirst.frc.team1759.robot.Sensors;
+import org.usfirst.frc.team1759.robot.subsystems.TankDrive;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 public class Go extends Command {
+	
+	/**
+	 * This point is the point that the robot is at when called upon; initially has a value of initialPoint.
+	 */
+	Point currentPoint;
+	/**
+	 * This is the point that the robot is at when the command begins. By default, this is set to the origin.
+	 */
+	Point initialPoint;
+	/**
+	 * This is the destination point. This is set by the function setDestination(). By default, this is the origin.
+	 */
+	Point dest;
+	/**
+	 * This is the heading to reach the destination point. This is calculated by the function setHeading().
+	 */
+	Vector2 heading;
+	/**
+	 * This vector is generated from the initial and destination points. It is then added to the initial point to generate the currentPoint.
+	 */
+	Vector2 changeVector;
+	Sensors sense;
+	TankDrive tank;
+	
+	double distance;
+	
+	double accelX;
+	double accelY;
+	double initAccelX = 0;
+	double initAccelY = 0;
+	double prevAccelX;
+	double prevAccelY;
+	
+	double positionX;
+	double positionY;
+	double initPositionX = 0;
+	double initPositionY = 0;
+	double prevPositionX;
+	double prevPositionY;
+	
+	double veloX;
+	double veloY;
+	double initVeloX = 0;
+	double initVeloY = 0;
+	double prevVeloX;
+	double prevVeloY;
+	
+	double timeInit;
+	double timeNow;
+	double prevTime;
+	long timeInMilli;
+	long timeInitInMilli;
+	
+	static final double range = 0.2;
+
+	
+	public Go(TankDrive tank, Point dest) {
+		tank = new TankDrive();
+		initialPoint = Constants.ORIGIN;
+		currentPoint = initialPoint;
+		dest = new Point(dest.x, dest.y);
+		sense = new Sensors();
+		timeInitInMilli = System.currentTimeMillis();
+		timeInit = timeInitInMilli * Constants.MILLI_TO_SEC;
+		prevAccelX = initAccelX;
+		prevAccelY = initAccelY;
+		prevPositionX = initPositionX;
+		prevPositionY = initPositionY;
+		prevTime = timeInit;
+		prevVeloX = initVeloX;
+		prevVeloY = initVeloY;
+	}
+	
+	/**
+	 * This function finds the vector that stretches between two points: The current point and the destination point.
+	 * @return
+	 */
+	public Vector2 setHeading() {
+		heading = currentPoint.vectorTo(dest);
+		return heading;
+	}
+	
+	/**
+	 * This function finds the length of the vector between the start and final point.
+	 * @return
+	 */
+	public void setDistance() {
+		distance = heading.length();
+	}
+	
+	/**
+	 * This function is a basic kinematics function, one which determines our position and returns it as a point.
+	 * @return
+	 */
+	public Point updateCurrentPoint() {
+		positionX = (.5 * accelX * timeNow * timeNow) + (((prevAccelX * prevTime) + prevVeloX) * timeNow) + prevPositionX;
+		positionY = (.5 * accelY * timeNow * timeNow) + (((prevAccelY * prevTime) + prevVeloY) * timeNow) + prevPositionY;
+		changeVector = new Vector2(positionX, positionY);
+		currentPoint = currentPoint.add(changeVector);
+		return currentPoint;
+	}
+	
+	/**
+	 * This functions looks at the accelerometer and the current time. This data is used in the above kinematics function.
+	 * @param sense
+	 */
+	public void getSensorValues(Sensors sense) {
+		accelX = sense.accelerometer.getX();
+		accelY = sense.accelerometer.getY();
+		timeInMilli = System.currentTimeMillis() - timeInitInMilli;
+		timeNow = timeInMilli * Constants.MILLI_TO_SEC;
+	}
+	
+	@Override
+	protected void execute() {
+		setDistance();
+		if(Math.abs(distance) > range) {
+			getSensorValues(sense);
+			updateCurrentPoint();
+			if (distance > range) {
+				tank.tankDrive(1.0, 1.0);
+			} else if (distance < range) {
+				tank.tankDrive(-1.0, -1.0);
+			}
+		}
+	}
 
 	@Override
 	public void setName(String subsystem, String name) {
@@ -11,7 +144,11 @@ public class Go extends Command {
 
 	@Override
 	protected boolean isFinished() {
+		if (Math.abs(distance) < range) {
+			return true;
+		} else {
 		return false;
+		}
 	}
 
 }
