c48ca71b9045f4ddb22f0b5a810757f6e1514eab
uakotaobi <uche.robotics@akotaobi.org>
2018-02-19 13:26:51 -0800

    Add a scall that draws the map, appropriately scaled, for whatever screen size.
    Right now all it draws are the borders.
    
    Colors are untested, but in theory they should work.

--- src/main/java/tests/WaypointSimulator.java
+++ src/main/java/tests/WaypointSimulator.java
@@ -2,12 +2,15 @@ package tests;
 
 public class WaypointSimulator {
 
-	/**
-	 * Runs the Waypoint Simulator. Also prints Hello World!
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+        /**
+         * Runs the Waypoint Simulator. Also prints Hello World!
+         * @param args
+         */
+        public static void main(String[] args) {
+
+                Map map = new Map();
+                map.draw(40, 20);
+                System.out.println("Hello World!");
+        }
 
 }
