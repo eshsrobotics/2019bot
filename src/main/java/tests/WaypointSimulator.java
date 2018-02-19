62055d00c6e1c806e64dc38328c62f02e18a356a
Aidan <asgalb15@gmail.com>
2018-02-19 10:59:51 -0800

    Add tests for simulation

---
+++ src/main/java/tests/WaypointSimulator.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class WaypointSimulator {
+
+	/**
+	 * Runs the Waypoint Simulator. Also prints Hello World!
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
