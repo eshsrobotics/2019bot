a1ae079cb8221279571d59d3305f2a46f64947e8
FRC1 <FRC1@HS-ENGLAPTOP3.ESUSD.esusd.k12.ca.us>
2019-02-09 09:12:19 -0800

    Cleaned and Cleansed code, 2/7/19. Removed unnecessary files to fix the NullPointerException. Deploys smoothly, look to future branches for driving code however.

--- src/main/java/org/usfirst/frc/team1759/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team1759/robot/Robot.java
@@ -7,34 +7,34 @@
 
 package org.usfirst.frc.team1759.robot;
 
-import org.usfirst.frc.team1759.robot.commands.FollowPath;
-import org.usfirst.frc.team1759.robot.subsystems.Arm;
-import org.usfirst.frc.team1759.robot.subsystems.Climber;
-import org.usfirst.frc.team1759.robot.subsystems.Intake;
-import org.usfirst.frc.team1759.robot.subsystems.Launcher;
+//import org.usfirst.frc.team1759.robot.commands.FollowPath;
+//import org.usfirst.frc.team1759.robot.subsystems.Arm;
+//import org.usfirst.frc.team1759.robot.subsystems.Climber;
+//import org.usfirst.frc.team1759.robot.subsystems.Intake;
+//import org.usfirst.frc.team1759.robot.subsystems.Launcher;
 import org.usfirst.frc.team1759.robot.subsystems.TankDrive;
 
 import edu.wpi.first.wpilibj.Spark;
 
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import models.Graph;
+//import models.Graph;
 
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends IterativeRobot {
+	
 	private TankDrive tank;
-	private Launcher launcher;
-	private Intake upperIntake;
-	private Intake lowerIntake;
-	private Climber climber;
-	private Arm arm;
+	//private Launcher launcher;
+	//private Intake upperIntake;
+	//private Intake lowerIntake;
+	//private Climber climber;
+	//private Arm arm;
 	private OI oi;
 	private MatchData matchData;
 	private Encoder encoder;
@@ -45,22 +45,22 @@ public class Robot extends IterativeRobot {
 	 */
 	private static final double THROTTLE_THRESHOLD = 1 / 3.0;	 
 
-
 	@Override
 	public void robotInit() {
+		System.out.println("Test \n");
 		// Streams usb camera video feed straight to Dashboard.
 		CameraServer.getInstance().startAutomaticCapture();
 		// Initialize drive.
 		oi = new OI();
 		tank = new TankDrive();
-		upperIntake = new Intake(new Spark(RobotMap.UPPER_LEFT_INTAKE),
-				new Spark(RobotMap.UPPER_RIGHT_INTAKE));
-		lowerIntake = new Intake(new Spark(RobotMap.LOWER_LEFT_INTAKE),
-				new Spark(RobotMap.LOWER_RIGHT_INTAKE));
-		arm = new Arm(new DoubleSolenoid(RobotMap.ARM_PORT_IN,
-				RobotMap.ARM_PORT_OUT));
-		launcher = new Launcher();
-		climber = new Climber();
+		//upperIntake = new Intake(new Spark(RobotMap.UPPER_LEFT_INTAKE),
+			//	new Spark(RobotMap.UPPER_RIGHT_INTAKE));
+		//lowerIntake = new Intake(new Spark(RobotMap.LOWER_LEFT_INTAKE),
+			//	new Spark(RobotMap.LOWER_RIGHT_INTAKE));
+		//arm = new Arm(new DoubleSolenoid(RobotMap.ARM_PORT_IN,
+			//	RobotMap.ARM_PORT_OUT));
+		//launcher = new Launcher();
+		//climber = new Climber();
 		// Parse match data for use later on
 		matchData = new MatchData(DriverStation.getInstance());
 		encoder = new Encoder(0, 1);		//TODO: Determine pulses per revolution and distance per revolution in order to set distance per pulse
@@ -68,7 +68,7 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void disabledInit() {
-
+System.out.println("Test2 \n");
 	}
 
 	public void disabledPeriodic() {
@@ -76,12 +76,13 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void autonomousInit() {
-		Graph graph = new Graph(matchData);
-		FollowPath followPath = new FollowPath(encoder, tank, graph.currentNode, Graph.findPath(graph.currentNode, graph.target));
-		followPath.start();
+		//Graph graph = new Graph(matchData);
+		//FollowPath followPath = new FollowPath(encoder, tank, graph.currentNode, Graph.findPath(graph.currentNode, graph.target));
+		//followPath.start();
 	}
 
 	public void autonomousPeriodic() {
+		System.out.println("Test3 \n");
 		Scheduler.getInstance().run();
 	}
 
@@ -93,22 +94,22 @@ public class Robot extends IterativeRobot {
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
 		tank.tankDrive(oi);
-		launcher.launch(oi);
-		climber.climb(oi);
+		//launcher.launch(oi);
+		//climber.climb(oi);
 		if(oi.intakeInJoystick.get() || oi.intakeInNetwork.get()) {
-			lowerIntake.takeIn(1.0);
-			upperIntake.takeIn(1.0);
+			//lowerIntake.takeIn(1.0);
+			//upperIntake.takeIn(1.0);
 		} else if(oi.intakeOutJoystick.get() || oi.intakeOutJoystick.get()) {
-			lowerIntake.pushOut(1.0);
-			upperIntake.pushOut(1.0);
+			//lowerIntake.pushOut(1.0);
+			//upperIntake.pushOut(1.0);
 		} else {
-			upperIntake.stop();
-			lowerIntake.stop();
+			//upperIntake.stop();
+			//lowerIntake.stop();
 		}
 		if(oi.rightJoystick.getThrottle() > THROTTLE_THRESHOLD) {
-				arm.raise();
+				//arm.raise();
 		} else if(oi.rightJoystick.getThrottle() < -1.0 * THROTTLE_THRESHOLD) {
-			arm.lower();
+			//arm.lower();
 		} else {
 			/**
 			 * If neither condition is true, the arm will remain in it's current state. If we utilize the kOff setting for the solenoid, it will move as
--- src/main/java/org/usfirst/frc/team1759/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc/team1759/robot/RobotMap.java
@@ -13,30 +13,19 @@ package org.usfirst.frc.team1759.robot;
 public class RobotMap {
 	
 	// Drive controller ports
-	public static int LEFT_FRONT_PORT = 6;
-	public static int LEFT_BACK_PORT = 0;
-	public static int RIGHT_FRONT_PORT = 7;
-	public static int RIGHT_BACK_PORT = 2;
-	
-	// For launcher solenoid
-	public static int HIGH_LAUNCH_PORT_OUT = 0;
-	public static int HIGH_LAUNCH_PORT_IN = 1;
-	
-	// For intake solenoid
-	public static int ARM_PORT_OUT = 3;
-	public static int ARM_PORT_IN = 2;
+	public static int LEFT_FRONT_PORT = 2;
+	public static int LEFT_BACK_PORT = 3;
+	public static int RIGHT_FRONT_PORT = 0;
+	public static int RIGHT_BACK_PORT = 1;
 	
 	// Upper intake motors
-	public static int UPPER_LEFT_INTAKE= 3;
-	public static int UPPER_RIGHT_INTAKE = 1;
+	//public static int UPPER_LEFT_INTAKE= 3;
+	//public static int UPPER_RIGHT_INTAKE = 1;
 	
 	// Lower intake motors
-	public static int LOWER_LEFT_INTAKE = 4;
-	public static int LOWER_RIGHT_INTAKE = 5;
+	//public static int LOWER_LEFT_INTAKE = 4;
+	//public static int LOWER_RIGHT_INTAKE = 5;
 	
-	// Climber ports
-	public static int CLIMBER_PORT_1 = 10; 		//Spark, not Talon
-	public static int CLIMBER_PORT_2 = 11;		//Spark, not Talon
 	
 	
 
